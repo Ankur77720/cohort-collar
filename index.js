858ddfc3b0531fe6840da715fc5911df74240edd
Asad Khan <104260581+Asad935477@users.noreply.github.com>
2025-04-09 21:26:45 +0530

    feat: add multiplication function

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function sum(a, b) {
   return a + b;
 }
 
-// Thought I Should add a substraction funtion
-function subtract(a, b) {
+function sub(a, b) {
   return a - b;
+}
+// Thought I Should add a Multiplication funtion
+function multi(a, b) {
+  return a * b;
 }
